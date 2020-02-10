8364661fe304a655021510c94738fe653ae7c8fe
Aman Gupta <amangupta884036@gmail.com>
2020-02-10 11:59:06 +0530

    listfield.php is Automatically committed

---
+++ PHP/H_S_Rana_Sir(Old_Syllabus)/PHP_File/Unit-3/functions/listfield.php
@@ -0,0 +1,13 @@
+
+<?php
+$result = mysql_query("SHOW COLUMNS FROM sometable");
+if (!$result) {
+    echo 'Could not run query: ' . mysql_error();
+    exit;
+}
+if (mysql_num_rows($result) > 0) {
+    while ($row = mysql_fetch_assoc($result)) {
+        print_r($row);
+    }
+}
+?>
