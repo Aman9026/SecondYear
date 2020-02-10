7ecf4b6260f3bdf7be3181833195b7849350482e
Aman Gupta <amangupta884036@gmail.com>
2020-02-10 11:41:05 +0530

    dbname.php is Automatically committed

---
+++ PHP/H_S_Rana_Sir(Old_Syllabus)/PHP_File/Unit-3/functions/dbname.php
@@ -0,0 +1,14 @@
+
+<?php
+error_reporting(E_ALL);
+
+$link = mysql_connect('localhost', 'root', '');
+$db_list = mysql_list_dbs($link);
+
+$i = 0;
+$cnt = mysql_num_rows($db_list);
+while ($i < $cnt) {
+    echo mysql_db_name($db_list, $i) . "<br>";
+    $i++;
+}
+?>
