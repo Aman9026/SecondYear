44ea13421212b39d00ddd9e6a5b8bf9662ec6bbc
Aman Gupta <amangupta884036@gmail.com>
2020-02-10 10:51:57 +0530

    private.php is Automatically committed

---
+++ PHP/H_S_Rana_Sir(Old_Syllabus)/PHP_File/OOPS/private.php
@@ -0,0 +1,18 @@
+<?php
+class Person
+{
+private $name;
+public function set_name($data)
+{
+$this->name=$data;
+}
+public function get_name()
+{
+return $this->name;
+}
+}
+$ralph=new Person;
+$ralph->set_name("srikanth");
+echo "<b>the name of your friend is:</b>",$ralph->get_name(),"<br>";
+echo "<b>the name of your friend is:</b>",$ralph->name,"<br>";
+?>
