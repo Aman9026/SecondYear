595efe7358ad2df6631a83cc3c6e1c0fe2ec6a0c
Aman Gupta <amangupta884036@gmail.com>
2020-02-10 12:02:06 +0530

    listtable.php is Automatically committed

---
+++ PHP/H_S_Rana_Sir(Old_Syllabus)/PHP_File/Unit-3/functions/listtable.php
@@ -0,0 +1,24 @@
+
+<?php
+$dbname = 'mysql_dbname';
+
+if (!mysql_connect('mysql_host', 'mysql_user', 'mysql_password')) {
+    echo 'Could not connect to mysql';
+    exit;
+}
+
+$sql = "SHOW TABLES FROM $dbname";
+$result = mysql_query($sql);
+
+if (!$result) {
+    echo "DB Error, could not list tables\n";
+    echo 'MySQL Error: ' . mysql_error();
+    exit;
+}
+
+while ($row = mysql_fetch_row($result)) {
+    echo "Table: {$row[0]}\n";
+}
+
+mysql_free_result($result);
+?>
