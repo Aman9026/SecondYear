d5f7b5975c92f483fbb8839c8a1fb2e6f9eff127
Aman Gupta <amangupta884036@gmail.com>
2020-02-10 09:55:46 +0530

    user.php is Automatically committed

---
+++ PHP/Alind_Sir(New_Syllabus)/Lab/Lab_2/Task_3/user.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<?php @$Number1 =$_POST['Number1'];
+@$Number2 =$_POST['Number2'];
+$Add=$Number1+$Number2;
+$Multiple=$Number1*$Number2;
+echo "Sum:",$Add;
+echo "";
+echo "Multiple:",$Multiple;
+;?>
+</body>
+</html>
