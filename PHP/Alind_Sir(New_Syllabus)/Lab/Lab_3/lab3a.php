2a6834f5fe745121aa807b430e1ae88e0342d222
Aman Gupta <amangupta884036@gmail.com>
2020-02-10 09:58:46 +0530

    lab3a.php is Automatically committed

---
+++ PHP/Alind_Sir(New_Syllabus)/Lab/Lab_3/lab3a.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+	<br><center>
+<?php @$Number =$_GET['Number'];
+if($Number%2==0)
+	{
+		echo "Number is Even";
+	}
+	else
+	{
+		echo "Number is Odd";
+	}?>
+</center>
+</body>
+</html>
