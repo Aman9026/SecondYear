468ff5222ef0a0ccecd3802b8e3a0ce33c30f8cd
Aman Gupta <amangupta884036@gmail.com>
2020-02-21 09:46:49 +0530

    classesandobjects.java is Automatically committed

---
+++ Java/Lab/EXP2/classesandobjects.java
@@ -0,0 +1,17 @@
+public class classesandobjects
+{
+
+	static class Hello
+	{
+	public void printhello()
+		{
+		System.out.println("Hello");
+		}
+	}
+
+public static void main(String args[])
+{
+	classesandobjects.Hello obj = new classesandobjects.Hello();
+	obj.printhello();
+}
+}
