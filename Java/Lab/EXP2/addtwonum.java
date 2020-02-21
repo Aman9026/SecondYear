107336a222a2e01b7bb2ef99621f1e2bd8341561
Aman Gupta <amangupta884036@gmail.com>
2020-02-21 09:41:49 +0530

    addtwonum.java is Automatically committed

---
+++ Java/Lab/EXP2/addtwonum.java
@@ -0,0 +1,11 @@
+public class addtwonum
+{
+public static void main(String args[])
+{
+int sum=0,num1=0,num2=0;
+num1=Integer.parseInt(args[0]);
+num2=Integer.parseInt(args[1]);
+sum=num1+num2;
+System.out.println(sum);
+}
+}
