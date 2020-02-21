ad5eeb769af51ef5565341e961e72d5f611cb0ef
Aman Gupta <amangupta884036@gmail.com>
2020-02-21 09:59:17 +0530

    possiblecomb.java is Automatically committed

---
+++ Java/Lab/EXP3/possiblecomb.java
@@ -0,0 +1,8 @@
+public class possiblecomb
+{
+public static void main(String args[])
+{
+int num1=Integer.parseInt(args[0]),num2=Integer.parseInt(args[1]),num3=Integer.parseInt(args[2]);
+System.out.print(" "+num3+num2+num1+" "+num2+num1+num3+" "+num3+num1+num2+" "+num1+num2+num3+" "+num2+num3+num1+" "+num1+num3+num2);
+}
+}
