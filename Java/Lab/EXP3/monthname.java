ae8bd3899cb855f03ff855317d12fe9da8d246d2
Aman Gupta <amangupta884036@gmail.com>
2020-02-21 09:57:17 +0530

    monthname.java is Automatically committed

---
+++ Java/Lab/EXP3/monthname.java
@@ -0,0 +1,19 @@
+public class monthname
+{
+public static void main(String args[])
+{
+int month= Integer.parseInt(args[0]);
+if(month==1){System.out.println("Jan");}
+if(month==2){System.out.println("Feb");}
+if(month==3){System.out.println("Mar");}
+if(month==4){System.out.println("Apr");}
+if(month==5){System.out.println("May");}
+if(month==6){System.out.println("June");}
+if(month==7){System.out.println("Jul");}
+if(month==8){System.out.println("Aug");}
+if(month==9){System.out.println("Sep");}
+if(month==10){System.out.println("Oct");}
+if(month==11){System.out.println("Nov");}
+if(month==12){System.out.println("Dec");}
+}
+}
