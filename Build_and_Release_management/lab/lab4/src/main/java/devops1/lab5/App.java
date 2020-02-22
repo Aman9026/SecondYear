e3da66e6017b5849fe1759208b405c50bd3afa95
Aman Gupta <amangupta884036@gmail.com>
2020-02-22 19:30:54 +0530

    Added App.java

---
+++ Build_and_Release_management/lab/lab4/src/main/java/devops1/lab5/App.java
@@ -0,0 +1,13 @@
+package devops1.lab5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
